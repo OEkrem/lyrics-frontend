a0176fd3f32dfaf06be90942af065987dc851a5b
Ekrem <oekremyildirim@outlook.com>
2025-04-30 17:42:23 +0300

    fix: fix lyrics API GET problem

--- src/api/lyricsApi.js
+++ src/api/lyricsApi.js
@@ -2,20 +2,10 @@
 
 import api from './apiClient';
 
-const url = '/artist/title';
-
 export const fetchLyrics = async (artist, title) => {
     try {
-        const params = {
-            artist: artist,
-            title: title,
-          };
-          if (artist) params.artist = artist;
-          if (title) params.title = title;
-          
-        const response = await api.get(url, {params});
-        return response.lyrics;
-
+        const response = await api.get(`/${artist}/${title}`);
+        return response.data.lyrics;
     } catch (error) {
         console.error("Error fetching lyrics:", error);
         return null;
